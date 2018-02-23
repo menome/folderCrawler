a91e199b567a036c7eff132253b7d88920e09ccb
Konrad Aust <konrad.aust@gmail.com>
2018-02-23 16:57:51 -0700

    Now streams results from the file command

--- app/crawler.js
+++ app/crawler.js
@@ -12,7 +12,9 @@ var path = require('path');
 var bot = require('@menome/botframework');
 var minioUploader = require('./minioUploader');
 var fs = require('fs');
-const { execFile } = require('child_process');
+const { execFile, spawn } = require('child_process');
+const readline = require('readline');
+const whitelist = new RegExp(conf.get("crawler.matchRegex"));
 
 module.exports = {
   CrawlFolder
@@ -20,54 +22,55 @@ module.exports = {
 
 // Crawls a folder. Runs queries to put the folder structure in the graph.
 // Copies the successfully merged files into a directory.
-function CrawlFolder(line, bucketDest, originPrefix, cb) {
-  line = path.normalize(line);
+function CrawlFolder(localCrawlDir, bucketDest, originPrefix, cb) {
+  localCrawlDir = path.normalize(localCrawlDir);
   bucketDest = path.posix.normalize(bucketDest);
-  bot.logger.info("Starting folder crawler on: " + line);
-  var filesToCopy = [];
+  bot.logger.info("Starting folder crawler on: " + localCrawlDir);
+
+  var findcmd = spawn("find", [localCrawlDir, "-type", "f", "-regextype", "grep", "-iregex", conf.get("crawler.findRegex")]);
+  var rl = readline.createInterface({ input: findcmd.stdout });
+
+  var workQueue = async.queue((task,callback) => {
+    task().then(()=>{ callback(); })
+  })
   
-  var whitelist = new RegExp(conf.get("crawler.matchRegex"));
-  
-  // Find all files
-  execFile("find", [line, "-type", "f", "-regextype", "grep", "-iregex", conf.get("crawler.findRegex")], (err, stdout, stderr) => {
-    if(err) return bot.logger.info(err.toString())
-    var files = stdout.split("\n");
+  findcmd.on("error", (err) => {
+    bot.logger.error(err.toString());
+  })
 
-    // Iterate through one at a time. Don't keep going until we run our query.
-    async.eachSeries(files, (file, next) => {
-      if(!file.match(whitelist)) { return next(); }
+  rl.on('line', (input) => {
+    workQueue.push(processFile.bind(this,{fileName: input, bucketDest, originPrefix, localCrawlDir}));
+  })
+}
 
-      // Process file here.
-      file = path.normalize(file);
-      var originPath = originPrefix + path.posix.normalize(file).replace(line,'');
-      var destFilePath = path.join(bucketDest,path.posix.normalize(file).replace(line,''))
-      var folderStructure = path.join(conf.get("minio.fileBucket"),destFilePath).split(path.sep).filter(itm=>!!itm) // Path split into an array of names.
 
-      // Run the query to add the file to the graph.
-      var query = queryBuilder.mergeFileAndSubdirQuery(folderStructure, file, originPath);
-      return bot.query(query.compile(), query.params()).then((itm) => {
-        bot.logger.info("Added file", file);
-        fileObj = {
-          dest: destFilePath,
-          loc: file,
-          mime: mime.lookup(file)
-        };
-        filesToCopy.push(fileObj);
-        return next();
-      }).catch((err) => {
-        bot.logger.error("Error merging subdirectory with graph: " + err.toString());
-        return next();
-      })
-    }, () => {
-      bot.logger.info("Done graphing: " + line + ". Copying " + filesToCopy.length + " files to minio");
-      // Copy the files that worked.
-      async.eachSeries(filesToCopy, (fileToCopy, next) => {
-        minioUploader.copyFilePromise(fileToCopy, bot.logger)
-        .then(function (res) {
-          bot.logger.info("copy file finished: " + res);
-          return next();
-        })
-      })
-    });
+// Updates the graph with the new file. Then uploads the file to Minio.
+// Returns a promise that resolves when both of these things are done.
+function processFile({fileName, bucketDest, originPrefix, localCrawlDir}) {
+  if(!fileName.match(whitelist)) { return Promise.resolve(false) }
+
+  // Process file here.
+  fileName = path.normalize(fileName);
+  var originPath = originPrefix + path.posix.normalize(fileName).replace(localCrawlDir,'');
+  var destFilePath = path.join(bucketDest,path.posix.normalize(fileName).replace(localCrawlDir,''))
+  var folderStructure = path.join(conf.get("minio.fileBucket"),destFilePath).split(path.sep).filter(itm=>!!itm) // Path split into an array of names.
+
+  // Run the query to add the file to the graph.
+  var query = queryBuilder.mergeFileAndSubdirQuery(folderStructure, fileName, originPath);
+  return bot.query(query.compile(), query.params()).then((itm) => {
+    bot.logger.info("Added file to graph. Now uploading to Minio:", fileName);
+
+    // Now build our file object for uploading to Minio
+    fileObj = {
+      dest: destFilePath,
+      loc: fileName,
+      mime: mime.lookup(fileName)
+    };
+
+    return minioUploader.copyFilePromise(fileObj, bot.logger).then(function (res) {
+      bot.logger.info("Copy file finished: " + res);
+    })
+  }).catch((err) => {
+    bot.logger.error("Error merging subdirectory with graph:", err.toString());
   })
 }
